05b31d4065170dd2a9352b37e5039ef978ccb5c5
damianwayne7 <69078764+damianwayne7@users.noreply.github.com>
2024-06-25 20:00:08 +0530

    Update upload_to_defectdojo.py

--- .github/workflows/upload_to_defectdojo.py
+++ .github/workflows/upload_to_defectdojo.py
@@ -1,12 +1,16 @@
 import json
-import os
 from defectdojo_api import defectdojo
+
+# Hardcoded DefectDojo connection details
+DEFECTDOJO_URL = "http://13.213.67.252:8080"
+DEFECTDOJO_API_KEY = "1b9c5ac045cfec60f3b508e969a6d3b7d3f52247"
+DEFECTDOJO_ENGAGEMENT_ID = "linter_scan"
 
 # Initialize the DefectDojo connection
 dojo = defectdojo.DefectDojoAPI(
-    host=os.environ["DEFECTDOJO_URL"],
-    api_key=os.environ["DEFECTDOJO_API_KEY"],
-    verify_ssl=False
+    host=DEFECTDOJO_URL,
+    api_key=DEFECTDOJO_API_KEY,
+    verify_ssl=False  # Change to True if using HTTPS and a valid SSL certificate
 )
 
 # Read the SARIF report
@@ -17,7 +21,7 @@ try:
 
     # Upload the report
     response = dojo.upload_sarif(
-        engagement_id=os.environ["DEFECTDOJO_ENGAGEMENT_ID"],
+        engagement_id=DEFECTDOJO_ENGAGEMENT_ID,
         file=report_content,
         scan_type='Sarif',
         active=True,
