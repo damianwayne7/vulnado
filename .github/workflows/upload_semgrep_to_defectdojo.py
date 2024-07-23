ddada18f58024ff7e85f1dca3bdb74726fcecc81
damianwayne7 <69078764+damianwayne7@users.noreply.github.com>
2024-07-23 21:29:09 +0530

    Update upload_semgrep_to_defectdojo.py

--- .github/workflows/upload_semgrep_to_defectdojo.py
+++ .github/workflows/upload_semgrep_to_defectdojo.py
@@ -1,7 +1,3 @@
-import requests
-import sys
-import os
-
 def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, filename):
     multipart_form_data = {
         'file': (filename, open(filename, 'rb')),
@@ -21,27 +17,3 @@ def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name,
     if r.status_code != 200:
         sys.exit(f'Post failed: {r.text}')
     print(r.text)
-
-if __name__ == "__main__":
-    try:
-        token = os.getenv("DEFECT_DOJO_API_TOKEN")
-    except KeyError:
-        print("Please set the environment variable DEFECT_DOJO_API_TOKEN")
-        sys.exit(1)
-    
-    if len(sys.argv) == 9:
-        url = sys.argv[2]
-        product_name = sys.argv[4]
-        engagement_name = sys.argv[6]
-        report = sys.argv[8]
-
-        print(f"URL: {url}")
-        print(f"Product Name: {product_name}")
-        print(f"Engagement Name: {engagement_name}")
-        print(f"Report: {report}")
-
-        uploadToDefectDojo(False, token, url, product_name, engagement_name, report)
-    else:
-        print(
-            'Usage: python3 upload_semgrep_to_defectdojo.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE')
-        sys.exit(1)
