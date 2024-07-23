3fcc2cb16cdffc26b14353c6b0a995bfaa2df038
damianwayne7 <69078764+damianwayne7@users.noreply.github.com>
2024-07-23 22:06:24 +0530

    Update upload_semgrep_to_defectdojo.py

--- .github/workflows/upload_semgrep_to_defectdojo.py
+++ .github/workflows/upload_semgrep_to_defectdojo.py
@@ -1,19 +1,44 @@
-def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, filename):
-    multipart_form_data = {
-        'file': (filename, open(filename, 'rb')),
-        'scan_type': (None, 'Semgrep JSON Report'),
-        'product_name': (None, product_name),
-        'engagement_name': (None, engagement_name),
-    }
+import requests
+import sys
+import os
 
-    endpoint = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
-    r = requests.post(
-        url + endpoint,
-        files=multipart_form_data,
-        headers={
-            'Authorization': 'Token ' + token,
-        }
-    )
-    if r.status_code != 200:
-        sys.exit(f'Post failed: {r.text}')
-    print(r.text)
+def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, filename):
+    try:
+        with open(filename, 'rb') as file:
+            multipart_form_data = {
+                'file': (filename, file),
+                'scan_type': (None, 'Semgrep JSON Report'),
+                'product_name': (None, product_name),
+                'engagement_name': (None, engagement_name),
+            }
+            endpoint = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
+            response = requests.post(
+                url + endpoint,
+                files=multipart_form_data,
+                headers={'Authorization': 'Token ' + token}
+            )
+            response.raise_for_status()  # Raise an error for HTTP error responses
+            print(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    try:
+        token = os.getenv("DEFECT_DOJO_API_TOKEN")
+        if not token:
+            raise ValueError("DEFECT_DOJO_API_TOKEN environment variable not set")
+        if len(sys.argv) != 9:
+            print('Usage: python3 upload_semgrep_to_defectdojo.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE')
+            sys.exit(1)
+
+        url = sys.argv[2]
+        product_name = sys.argv[4]
+        engagement_name = sys.argv[6]
+        report = sys.argv[8]
+
+        uploadToDefectDojo(False, token, url, product_name, engagement_name, report)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
